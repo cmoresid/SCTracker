f404da965bb768993b2a6513ff0f549af335c57e
mingzhen <mingzhen@ualberta.ca>
2012-02-07 18:42:05 -0700

    change

--- src/ca/ualberta/ProjectActivity.java
+++ src/ca/ualberta/ProjectActivity.java
@@ -12,6 +12,6 @@ public class ProjectActivity extends Activity {
         
         //this is a change from david!!
         //This is a change from Shaun.
-        
+        //there is a confilct
     }
 }
