43af14e06038268f5014477403356972df2e1cff
cmoresid <cmoreside@ualberta.ca>
2012-02-07 18:33:48 -0600

    Initial commit.

---
+++ src/ca/ualberta/ProjectActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
