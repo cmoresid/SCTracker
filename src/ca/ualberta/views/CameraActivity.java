3747a8d7f425dcea722bebabb39ce52979efda10
shstreep <shstreep@ualberta.ca>
2012-03-15 22:03:45 -0600

    Updating my project

--- src/ca/ualberta/views/CameraActivity.java
+++ src/ca/ualberta/views/CameraActivity.java
@@ -1,5 +1,6 @@
 package ca.ualberta.views;
 
+import ca.ualberta.R;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,6 +10,9 @@ public class CameraActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.camera_activity);
+		
 	}
 
 }
--- src/ca/ualberta/views/CompareActivity.java
+++ src/ca/ualberta/views/CompareActivity.java
@@ -1,14 +1,24 @@
 package ca.ualberta.views;
 
+import ca.ualberta.R;
+import ca.ualberta.models.PhotoEntry;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class CompareActivity extends Activity {
+	
+	// literally the photo on the upper half of the compare screen.
+	private PhotoEntry topPhoto;
+	
+	private PhotoEntry bottomPhoto;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.compare_activity);
+		
 	}
 	
 }
