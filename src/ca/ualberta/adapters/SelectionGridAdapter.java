37c85076186faabc3a77ffaa6dc00b73ed78e1a4
cmoresid <cmoreside@ualberta.ca>
2012-04-05 11:56:20 -0600

    Fixed documentation again. All of it accidently got deleted.

--- src/ca/ualberta/adapters/SelectionGridAdapter.java
+++ src/ca/ualberta/adapters/SelectionGridAdapter.java
@@ -19,29 +19,79 @@ import android.widget.TextView;
 import ca.ualberta.R;
 import ca.ualberta.models.PhotoEntry;
 
-public class SelectionGridAdapter extends BaseAdapter {
 
+/**	  	
+ * Reusable adapter that can be used to display	  	
+ * {@code PhotoEntry} images, dates, along with	  	
+ * a checkbox underneath to allow the selection 	
+ * of a particular photo. So far, this adapter  	
+ * is used in the CompareSelectionActivity and  	
+ * ArchiveActivity.	  	
+ */
+public class SelectionGridAdapter extends BaseAdapter {
+	
+	/** Shared reference to all photos with given tag. */
 	private ArrayList<PhotoEntry> mPhotos;
+	/**  	
+	 * Each boolean refers a photo's checkbox. Either checked	  	
+	 * or not check.  	
+	 */
 	private ArrayList<Boolean> mSelected;
-	private int fixedChecked;
+	/**	  	
+	  * Refers to the position of the checkbox that   	
+	  * is fixed. 	  	
+	  */
+	private int positionFixedChecked;
+	/** Inflates the layouts. */
 	private LayoutInflater mInflater;
+	/** Context is needed in order to retrieve layout inflater. */
 	private Context mContext;
+	/** The listener to attach to each checkbox. */
 	private OnClickListener mCurrentListener;
 	
+    /**	  	
+      * Instantiates a new {@code SelectionGridAdapter} with a context, 	
+      * a list of {@code PhotoEntry} objects, and a list of checkbox positions. 	
+      * 	
+      * @param c	
+      *     Context that is used to retrieve a LayoutInflater efficiently.	
+      * @param list
+      *     A shared list of {@code PhotoEntry} objects that are used to populate
+      *     the adapter.	
+      * @param selected 	
+      *     A shared list of booleans, where each position refers to the state  	
+      *     of a checkbox for a photo cell. 	
+      */
 	public SelectionGridAdapter(Context c, ArrayList<PhotoEntry> list, ArrayList<Boolean> selected) {
-		fixedChecked = -1;
+		positionFixedChecked = -1;
 		mPhotos = list;
 		mSelected = selected;
 		mContext = c;
 		mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 	
-	public void setOnClickListener(OnClickListener l) {
-		mCurrentListener = l;
+	/**  	
+	  * Sets the listener for the checkbox of each photo cell.  	
+	  *   	
+	  * @param listener  	
+	  *     The listener that is called when the checkbox is	  	
+	  *      clicked.  	
+	  */
+	public void setOnClickListener(OnClickListener listener) { 	
+    	mCurrentListener = listener;
 	}
 	
+
+    /**  	
+      * Sets the position of the checkbox that is to be 	
+      * checked, but don't allow the user to change it's 	
+      * state. 	
+      *  	
+      * @param pos
+      *     Position of checkbox that is to be fixed. 	
+      */
 	public void setFixedChecked(int pos) {
-		fixedChecked = pos;
+		positionFixedChecked = pos;
 	}
 	
 	@Override
@@ -73,6 +123,7 @@ public class SelectionGridAdapter extends BaseAdapter {
 			
 			Bitmap image = null;
 			try {
+				// Decode the image from file name found in photo entry.
 				image = BitmapFactory.decodeStream(mContext.openFileInput(entry.getFilePath()));
 			} catch (FileNotFoundException e1) {
 				Log.i("ProblemOpeningFile", entry.getFilePath());
@@ -93,9 +144,13 @@ public class SelectionGridAdapter extends BaseAdapter {
 		holder.imageView.setId(position);
 		
 		holder.checkBox.setOnClickListener(mCurrentListener);
+		// Set the checkbox's state to reflect the state of the 	
+		// mSelected array.
 		holder.checkBox.setChecked(mSelected.get(position));
-		
-		if (fixedChecked == position) {
+
+		// If this photocell should be locked,	  	
+		// lock it here.
+		if (positionFixedChecked == position) {
 			holder.checkBox.setEnabled(false);
 		}
 		
@@ -104,6 +159,7 @@ public class SelectionGridAdapter extends BaseAdapter {
 		return convertView;
 	}
 	
+	/** Used to cache a view. Helps efficiency. */
 	static class ViewHolder {
 		ImageView imageView;
 		CheckBox checkBox;
--- src/ca/ualberta/controllers/ArchiveController.java
+++ src/ca/ualberta/controllers/ArchiveController.java
@@ -1,30 +1,76 @@
 package ca.ualberta.controllers;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import android.os.Environment;
 
 import ca.ualberta.models.PhotoEntry;
 
+/**
+ * Implementation of the {@link SCController} class. Acts as the
+ * controller for ArchiveSelectionActivity. It subclasses
+ * {@link BaseSelectionController} so that it has the ability
+ * to retrieve photos, and populate the grid view. This
+ * controller is also responsible for actual process of archiving
+ * photos to the SD card.
+ */
 public class ArchiveController extends BaseSelectionController {
 
+	/**
+	 * Message code that tells controller to archive photos to
+	 * the SD card.
+	 */
 	public static final int ARCHIVE_PHOTOS = 4;
+	/**
+	 * Message code that is sent to any handlers that
+	 * archiving has been completed.
+	 */
 	public static final int ARCHIVE_RESULTS = 5;
 	
+	/**
+	 * Instantiates a new {@code ArchiveController} with a
+	 * shared {@code PhotoEntry} list, shared list of booleans,
+	 * and the tag of the photos to be displayed.
+	 * 
+	 * @param photos
+	 * 		A shared list of {@code PhotoEntry} that contains
+	 * 		all photos with a particular tag.
+	 * @param selected
+	 * 		A shared list of booleans that refer to the state
+	 * 		of all the checkboxes associated with the photos.
+	 * @param tag
+	 * 		The tag of the photos to be displayed.
+	 */
 	public ArchiveController(ArrayList<PhotoEntry> photos,
 			ArrayList<Boolean> selected, String tag) {
 		super(photos, selected, tag);
 	}
-
-	public void archivePhotos(ArrayList<PhotoEntry> entries) {
+	
+	public boolean sdCardMounted(){
+		return(Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED);	
+	}
+	
+	
+	/**
+	 * Archives the photos.
+	 * 
+	 * @param entries
+	 */
+	public int archivePhotos(final ArrayList<PhotoEntry> entries) {
 		inboxHandler.post(new Runnable() {
 			@Override
 			public void run() {
-				notifyOutboxHandlers(ARCHIVE_RESULTS, null);
+				notifyOutboxHandlers(ARCHIVE_RESULTS, null);	
 			}
 		});
+		return 0;
 	}
 	
 	@Override
 	public boolean handleMessage(int what, Object data) {
+		// If the parent can't handle it, let the ArchiveController
+		// try to handle the message.
 		if (!super.handleMessage(what, data)) {
 			switch (what) {
 			case ARCHIVE_PHOTOS:
