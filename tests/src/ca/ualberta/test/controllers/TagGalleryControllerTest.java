574a20e971a6308c75a39a3e778ad7578c0ddf48
dgulyas <dgulyas@ualberta.ca>
2012-04-06 12:51:40 -0600

    Started writing TagGalleryControllerTest

---
+++ tests/src/ca/ualberta/test/controllers/TagGalleryControllerTest.java
@@ -0,0 +1,11 @@
+package ca.ualberta.test.controllers;
+
+import android.test.AndroidTestCase;
+import ca.ualberta.controllers.TagGalleryController;
+
+public class TagGalleryControllerTest
+{
+	private TagGalleryController subject = new TagGalleryController(tags)
+	
+
+}
